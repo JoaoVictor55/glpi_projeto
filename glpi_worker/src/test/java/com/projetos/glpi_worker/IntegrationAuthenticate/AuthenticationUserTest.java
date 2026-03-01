791f2f844a57a7adcadfef9cf64724e6ad23bfec
= <=>
2026-03-01 19:09:47 -0300

    refactor: remove imports não utilizados

--- glpi_worker/src/test/java/com/projetos/glpi_worker/IntegrationAuthenticate/AuthenticationUserTest.java
+++ glpi_worker/src/test/java/com/projetos/glpi_worker/IntegrationAuthenticate/AuthenticationUserTest.java
@@ -3,7 +3,6 @@ package com.projetos.glpi_worker.IntegrationAuthenticate;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 import com.projetos.glpi_worker.service.api_authentication.AuthenticateWithPassword;
 
